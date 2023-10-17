5bbc19773b78ea2256a45e66bed8308c9098966a
freddiebell88 <frederikcwbell@gmail.com>
2023-10-17 15:36:50 -0400

    added all customer data

--- scripts.js
+++ scripts.js
@@ -14,21 +14,32 @@ const customerContainer = document.querySelector('#directory')
 // location.city.state.postcode
 function buildCustomers(customerArray) {
     for (let customer of customerArray) {
+        //create a div container for each box
         let box = document.createElement('div')
+        box.classList.add("customerCard")
+        //create div for photo and add to box
         let photo = document.createElement('img')
         photo.src = customer.picture.medium
         box.appendChild(photo)
-        box.classList.add("customerCard")
+        //create div for name and add to box
         let customerName = document.createElement('h3')
         customerName.innerText = (customer.name.first + " " + customer.name.last)
         box.appendChild(customerName)
         customerContainer.appendChild(box)
+        //create div for email and add to box
         let custEmail = document.createElement('div')
         custEmail.innerText = customer.email
         box.appendChild(custEmail)
+        //create div for address and add to box
         let custAddress = document.createElement('div')
-        custAddress.innerText = (customer.location.street.number + " " + customer.location.street.name + " " + customer.location.city)
+        custAddress.innerText = `${customer.location.street.number} ${customer.location.street.name} \n ${customer.location.city}, ${customer.location.state} ${customer.location.postcode}`
         box.appendChild(custAddress)
+        let birthdate = document.createElement('div')
+        birthdate.innerText = `DOB: ${customer.dob.date}`
+        box.appendChild(birthdate)
+        let customerSince = document.createElement('div')
+        customerSince.innerText = `Customer since: ${customer.registered.date}`
+        box.appendChild(customerSince)
     }
 }
 buildCustomers(customers)
